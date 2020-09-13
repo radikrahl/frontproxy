9b210df828e5322e66df8c69a41d659ad7de9a23
andreas.krahl <andreas.krahl@netzkern.de>
2020-09-13 21:05:55 +0200

    added frontend

--- default.vhost/default/src/app/app.module.ts
+++ default.vhost/default/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExternalLinkDirective } from './externallink.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExternalLinkDirective
   ],
   imports: [
     BrowserModule,
